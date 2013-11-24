3f05f76dfaddf0c75911b142108a94818b395aba
Stephen Li <stephen.liziyun@gmail.com>
2013-11-24 01:06:27 -0500

    HW6: store_current is buggy

--- hw6assignment.rb
+++ hw6assignment.rb
@@ -5,16 +5,65 @@
 
 class MyPiece < Piece
   # The constant All_My_Pieces should be declared here
+  All_My_Pieces = All_Pieces +
+              # addition pieces
+              [[[[0, 0], [-2, 0], [-1, 0], [1, 0], [2, 0]], # extra long (two cases)
+              [[0, 0], [0, -2], [0, -1], [0, 1], [0, 2]]],
+              rotations([[0, 0], [1, 0], [0, 1], [1, 1], [2, 0]]),
+              rotations([[0, 0], [1, 0], [0, 1]])]
 
   # your enhancements here
+
+  def initialize (point_array, board)
+    super(point_array, board)
+    @point_count = (@all_rotations[@rotation_index]).size
+  end
+
+  def point_count
+    @point_count
+  end
+
+  def self.next_piece (board)
+    MyPiece.new(All_My_Pieces.sample, board)
+  end
 
 end
 
 class MyBoard < Board
+
+  def initialize(game)
+    @grid = Array.new(num_rows) {Array.new(num_columns)}
+    @current_block = MyPiece.next_piece(self)
+    @score = 0
+    @game = game
+    @delay = 500
+  end
+
   # your enhancements here
   def rotate_180
     rotate_counter_clockwise
     rotate_counter_clockwise
+  end
+
+  # gets the next piece
+  def next_piece
+    @current_block = MyPiece.next_piece(self)
+    @current_pos = nil
+  end
+
+  # gets the information from the current piece about where it is and uses this
+  # to store the piece on the board itself.  Then calls remove_filled.
+  def store_current
+    locations = @current_block.current_rotation
+    point_count = @current_block.point_count
+    displacement = @current_block.position
+    (0..point_count).each{ |index|
+      current = locations[index];
+      @grid[current[1]+displacement[1]][current[0]+displacement[0]] =
+      @current_pos[index]
+    }
+    remove_filled
+    @delay = [@delay - 2, 80].max
   end
 end
 
